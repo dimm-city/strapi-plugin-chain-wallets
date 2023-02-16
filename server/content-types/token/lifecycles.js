15922a964942138ece7c81ad906b49ba04b615c2
itlackey <itlackey@outlook.com>
2023-02-16 02:31:04 -0600

    auto connect/create entity when token is created

--- server/content-types/token/lifecycles.js
+++ server/content-types/token/lifecycles.js
@@ -11,16 +11,54 @@ module.exports = {
       const contract = await svc.findOne(
         data?.contract?.id ?? data.contract.connect[0]?.id
       );
-      if (contract?.slug) {
-        data.slug = `${contract.slug}-${data.tokenId}`;
+      if (contract?.slug && !data.tokenId?.startsWith(contract.slug)) {
+        data.tokenId = `${contract.slug}-${data.tokenId}`;
       }
+    }
+  },
+  async afterCreate(event) {
+    const { result } = event;
+    const { data } = event.params;
+
+    if (data?.contract?.id || data?.contract?.connect?.length > 0) {
+      const svc = strapi.service(TYPE_CONTRACT);
+
+      //ToDo: refactor to only query if contract is null
+      const contract = await svc.findOne(
+        data?.contract?.id ?? data.contract.connect[0]?.id
+      );
       if (contract?.entityType) {
         const entitySvc = strapi.services[contract.entityType];
 
         if (entitySvc) {
           const functionName = contract.entityInitializer ?? NAME_ENTITY_INIT;
           if (entitySvc[functionName] instanceof Function) {
-            result = await entitySvc[functionName](data);
+            await entitySvc[functionName](result);
+          } else {
+            const entity = await entitySvc.find({
+              filters: {
+                tokenId: result.tokenId,
+              },
+            });
+
+            if (entity?.results?.length === 0) {
+              //Do default init, parse properties of metadata and attempt to map them to entity properties
+              await strapi.entityService.create(contract.entityType, {
+                data: {
+                  name: result.metadata?.name,
+                  tokenId: result.tokenId,
+                  token: result,
+                },
+              });
+            } else if (entity?.results?.length === 1) {
+              const id = entity?.results[0].id;
+
+              await strapi.entityService.update(contract.entityType, id, {
+                data: {
+                  token: result,
+                },
+              });
+            }
           }
         }
       }
