25694866dde139e825be6c0c41eb1a5ee09db3be
itlackey <itlackey@gmail.com>
2023-09-17 01:26:17 -0500

    cron setting and npm update

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -2,5 +2,8 @@
 const cronTasks = require("./config/cron-tasks");
 module.exports = ({ strapi }) => {
   // bootstrap phase
-  strapi.cron.add(cronTasks);
+  const enableCron = strapi.config.get(
+    "plugins.chain-wallets.options.enableCron"
+  );
+  if (enableCron == true) strapi.cron.add(cronTasks);
 };
--- server/config/index.js
+++ server/config/index.js
@@ -4,7 +4,8 @@ module.exports = {
   default: ({ env }) => ({
     assetPath: env("CW_ASSET_PATH") ?? ".tokens",
     verificationMessage: "sign in",
-    infuraProjectId: env("infura_project_id") ?? "INFURA PROJECT ID"
+    infuraProjectId: env("infura_project_id") ?? "INFURA PROJECT ID",
+    enableCron: false
   }),
   validator() {},
 };
